384c67784177ca5eb3d218f4276f00584a8b8743
sppl <sppl@users.noreply.github.com>
2017-07-14 21:38:33 +0300

    add http://explorer.iticoin.com/
    http://explorer.iticoin.com/

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -67,9 +67,9 @@ void OptionsModel::Init()
         if(fTestNet)
             settings.setValue("strThirdPartyTxUrls", "");
         else
-            settings.setValue("strThirdPartyTxUrls", "http://explorer.super-ppl.com/tx/%s");
+            settings.setValue("strThirdPartyTxUrls", "http://explorer.iticoin.com/tx/%s");
     }
-    strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "http://explorer.super-ppl.com/tx/%s").toString();
+    strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "http://explorer.iticoin.com/tx/%s").toString();
     fMinimizeToTray = settings.value("fMinimizeToTray", false).toBool();
     fMinimizeOnClose = settings.value("fMinimizeOnClose", false).toBool();
     fCoinControlFeatures = settings.value("fCoinControlFeatures", false).toBool();
